2064eb7be281456dc31a83bb8f25257262bc9017
mazzama <mazzamabduljabbar@gmail.com>
2024-11-16 11:14:15 +0700

    [neovim] feat: add dap & vim-tmux-navigator

--- nvim/lua/azzam/core/options.lua
+++ nvim/lua/azzam/core/options.lua
@@ -18,8 +18,8 @@ vim.opt.clipboard = "unnamed"
 vim.opt.scrollbind = false
 vim.opt.wildmenu = true
 
-vim.opt.tabstop = 2
-vim.opt.shiftwidth = 2
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
 
 -- filetype related 
 vim.api.nvim_create_autocmd("FileType", {
